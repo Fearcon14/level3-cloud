9ac9a7dba7cbf5e9046462e603d04bcbe7c897e5
Kevin Sinn <kevinsinn01@gmail.com>
2026-02-02 10:29:04 +0000

    Fixed horrible missspelling

--- Week3/variables.tf
+++ Week3/variables.tf
@@ -1,5 +1,5 @@
 variable "service_account_key_path" {
-	description = "StackIT service account key (Key Flow auth)"
+	description = "STACKIT service account key (Key Flow auth)"
 	type        = string
 	sensitive   = true
 	default     = "service_account_key.json"

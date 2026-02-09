513ae2094090c4fc2d562c0fc9b0c9bc18aaa191
kevin Sinn <kevinsinn01@gmail.com>
2026-02-09 14:07:09 +0100

    added ListInstances handler

--- Week4_API/internal/api/handlers.go
+++ Week4_API/internal/api/handlers.go
@@ -1,13 +1,33 @@
 package api
 
 import (
+	"fmt"
+	"log/slog"
 	"net/http"
-	"github.com/Fearcon14/level3-cloud/Week4_API/internal/models"
 
+	"github.com/Fearcon14/level3-cloud/Week4_API/internal/k8s"
 	"github.com/labstack/echo/v5"
 )
 
 // Application holds dependencies for our handlers (e.g. K8s client, Logger etc.)
 type Application struct {
-	// K8sClient goes here
+	Store  k8s.InstanceStore
+	Logger *slog.Logger
+}
+
+func NewApplication(store k8s.InstanceStore, logger *slog.Logger) *Application {
+	return &Application {
+		Store: store,
+		Logger: logger,
+	}
+}
+
+func (a *Application) ListInstances(c echo.Context) error {
+	ctx := c.Request().Context()
+	instances, err := a.Store.ListInstances(ctx)
+	if err != nil {
+		a.Logger.Error("failed to list instances", "error", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": fmt.Errorf("failed to list instances").Error()})
+	}
+	return c.JSON(http.StatusOK, instances)
 }
